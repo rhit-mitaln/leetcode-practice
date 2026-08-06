df7588ce70b97e1309d4527e47c247b3746bf2ca
Nakul Mital <mitaln@rose-hulman.edu>
2026-08-06 13:55:52 +0530

    Time: 7 ms (11.15%), Space: 44.5 MB (99.95%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        String s = String.valueOf(x);
+        return isPalindromeHelper(s);
+    }
+    public boolean isPalindromeHelper(String s){
+       if (s.length() <= 1) {
+            return true; 
+        }
+
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        return isPalindromeHelper(s.substring(1, s.length() - 1));
     }
 }
