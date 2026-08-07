f0928910cc5466db5e6fb31d5013720290254545
Nakul Mital <mitaln@rose-hulman.edu>
2026-08-07 15:08:38 +0530

    Time: 0 ms (100%), Space: 42.1 MB (99.93%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int len = s.length()-1;
+
+        while (s.charAt(len) == ' ') {
+            len--;
+        }
+
+        for (int i = len; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count; 
+    }
+}
